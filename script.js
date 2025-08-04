f1a2a71d4e3e44995380114916240c9e9320d80e
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-08-05 07:02:03 +0800

    save

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function fibs(number){
+function fibs(n){
+    let array = [0, 1];
+    let prev1 = array [0];
+    let prev2 = array [1];
+
+    for (let i = 0; i< n-2; i++ ){
+        let newFib = prev1 + prev2;
+        array.push(newFib)
+        console.log(array);
+        
+        prev1 = prev2;
+        prev2 = newFib
+    }
+}
+fibs(8)
+
+function fibsRec(){
     
 }
